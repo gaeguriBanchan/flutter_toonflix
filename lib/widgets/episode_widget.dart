5fbf57bbcca639d886f3abf03fc10defc1e8479b
gaeguriBanchan <rlawowls93@gamil.com>
2023-08-10 18:06:18 +0900

    Url Launcher, episode 클릭시 웹툰으로 이동

---
+++ lib/widgets/episode_widget.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_toonflix/models/webtoon_episode_model.dart';
+import 'package:url_launcher/url_launcher_string.dart';
+
+class Episode extends StatelessWidget {
+  const Episode({
+    super.key,
+    required this.episode,
+    required this.webtoonId,
+  });
+  final webtoonId;
+  final WebtoonEpisodeModel episode;
+
+  onButtonTap() async {
+    await launchUrlString(
+        'https://comic.naver.com/webtoon/detail?titleId=$webtoonId&no=${int.parse(episode.id) + 1}');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onButtonTap,
+      child: Container(
+        margin: const EdgeInsets.only(
+          bottom: 5,
+        ),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(20),
+          color: Colors.green[400],
+        ),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(
+            vertical: 10,
+            horizontal: 20,
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                episode.title,
+                style: const TextStyle(
+                  color: Colors.white,
+                  fontSize: 16,
+                ),
+              ),
+              const Icon(
+                Icons.chevron_right_rounded,
+                color: Colors.white,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
